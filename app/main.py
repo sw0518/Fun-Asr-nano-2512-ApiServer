5384759441a78bf0366d1eb4972411dcaeb45fb5
Mr.sun <sunwang0518@163.com>
2026-01-10 17:25:09 +0800

    优化docker部署

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ async def health_check():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8800, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
